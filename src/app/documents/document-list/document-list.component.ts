2ba40bffedace948b828d42ee7713d38098cf0cc
Yu-Chun <dyj0183@byui.edu>
2021-01-30 22:29:12 -0700

    week4

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'cms-document-list',
@@ -6,6 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./document-list.component.css']
 })
 export class DocumentListComponent implements OnInit {
+
+  allDocuments: Document[] = [
+    new Document ("WDD 430 Full-Stack Development", "Learn to use Angular", "https:sdfdsfdsfsd"),
+    new Document ("CSE 310 Applied Programming", "Explore different programming languages", "https:/dfsdfdsfsd"),
+    new Document ("CSE 340 Web Backend Development", "Learn about PHP & SQL", "https:/sfsdfds")
+  ]
+
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
+
+  onSelectedDocument(document: Document) {
+    this.selectedDocumentEvent.emit(document);
+
+  }
+
 
   constructor() { }
 
---
+++ src/app/documents/document.model.ts
@@ -0,0 +1,10 @@
+export class Document {
+
+  constructor(
+    public name: string,
+    public description: string,
+    public url: string
+  ) {}
+
+
+}
